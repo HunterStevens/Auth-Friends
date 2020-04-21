f2d56c3d3994ea5499dd683e29a3274d304e36f7
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-21 14:05:56 -0600

    Inputed the form for the Login Component

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -3,5 +3,25 @@ import axios from 'axios';
 import {authorize} from '../utils';
 import {connect} from '';
 
-const
+export const Login = () =>{
+
+
+    return(
+        <div className="Login-Form">
+             <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+        </div>
+    )
+}
 
